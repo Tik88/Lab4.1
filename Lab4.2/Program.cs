03b33954da4b9b5166d112128c187132c8b95bb6
Tik88 <xtikx@yandex.ru>
2021-10-24 13:25:10 +0300

    1

--- Lab4.2/Program.cs
+++ Lab4.2/Program.cs
@@ -10,7 +10,34 @@ namespace Lab4._2
     {
         static void Main(string[] args)
         {
-            
+            #region(Ввод данных)
+            Console.Write("Введите A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите C: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int k = 0;
+            int n = 0;
+            #endregion
+            //квадраты на стороне А
+            while (a >= c)
+            {
+                a -= c;
+                k++;
+            }
+            //квадраты на стороне В
+            while (b >= c)
+            {
+                b -= c;
+                n++;
+            }
+            int count = 0;
+            for (int i = 0; i < k; i++)
+                count += n;
+            Console.WriteLine("Количество квадратов = " + count);
+            Console.ReadKey();
         }
+
     }
 }
